d6bc27b5374dbc2cbc29bf58c5ccec088a1771f2
standen1 <standen.sean@gmail.com>
2022-10-17 21:59:42 +0000

    Fixed error.

--- components/topLevel/ErrorBoundary/ErrorBoundary.test.js
+++ components/topLevel/ErrorBoundary/ErrorBoundary.test.js
@@ -3,15 +3,17 @@ import '@testing-library/jest-dom';
 import ErrorBoundary from './ErrorBoundary';
 
 describe('Error Boundary', () => {
-    test('Error Boundary', () => {
-      const ThrowError = () => {
-        throw new Error('Test');
-      };
-    render(
-      <ErrorBoundary fallback={<ErrorBoundary />}>
-        <ThrowError />
-      </ErrorBoundary>
-    );
-    expect(screen.getByTestId('errorBoundary')).toBeVisible();
-    });
-  });
+	test('Error Boundary', () => {
+		const ThrowError = () => {
+			throw new Error('Test');
+		};
+
+		render(
+			<ErrorBoundary fallback={<ErrorBoundary />}>
+				<ThrowError />
+			</ErrorBoundary>
+		);
+
+		expect(screen.getByTestId('errorboundary')).toBeVisible();
+	});
+});
