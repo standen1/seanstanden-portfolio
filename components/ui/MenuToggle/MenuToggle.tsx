0c8a7d1e4daba47b6e3c6a30963305c60406db09
standen1 <standen.sean@gmail.com>
2022-10-25 21:33:17 +0000

    Fixed MenuToggle component SVG icon issues.

--- components/ui/MenuToggle/MenuToggle.tsx
+++ components/ui/MenuToggle/MenuToggle.tsx
@@ -22,12 +22,14 @@ export default function MenuToggle({ toggle, isOpen }: MenuToggleProps) {
     <button data-testid="button" className={styles.MenuToggle} onClick={toggle}>
         <svg width="23" height="23" viewBox="0 0 23 23">
         <Path
+          animate={isOpen ? "open" : "closed"}
           variants={{
             closed: { d: "M 2 2.5 L 20 2.5" },
             open: { d: "M 3 16.5 L 17 2.5" }
           }}
         />
         <Path
+          animate={isOpen ? "open" : "closed"}
           d="M 2 9.423 L 20 9.423"
           variants={{
             closed: { opacity: 1 },
@@ -36,6 +38,7 @@ export default function MenuToggle({ toggle, isOpen }: MenuToggleProps) {
           transition={{ duration: 0.1 }}
         />
         <Path
+          animate={isOpen ? "open" : "closed"}
           variants={{
             closed: { d: "M 2 16.346 L 20 16.346" },
             open: { d: "M 3 2.5 L 17 16.346" }
