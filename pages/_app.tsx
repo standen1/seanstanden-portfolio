30f0e3e8ea109f2f6e76bea223d28c3333341a3c
standen1 <standen.sean@gmail.com>
2022-10-29 02:10:40 +0000

    Fixed error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import styled, {ThemeProvider} from 'styled-components';
 import type { AppProps } from 'next/app';
+import { Poppins } from '@next/font/google';
 
 import { theme } from '../styles/colors'
 import { GlobalStyles } from '../styles/globals';
 import ErrorBoundary from '../components/topLevel/ErrorBoundary/ErrorBoundary';
 import Layout from '../components/topLevel/Layout/Layout';
+
+//Font Configuratio
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
