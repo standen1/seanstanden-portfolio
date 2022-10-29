80205d6efed706b4cc92b72e5357ab223960b2dc
standen1 <standen.sean@gmail.com>
2022-10-29 02:06:21 +0000

    Still working on styling for popover component.

--- components/ui/PortfolioPopover/PortfolioPopover.tsx
+++ components/ui/PortfolioPopover/PortfolioPopover.tsx
@@ -12,7 +12,7 @@ export interface PortfolioPopoverProps {
 }
 
 export default function PortfolioPopover({title, technologyStack, siteUrl, codeUrl}: PortfolioPopoverProps) {
-    const techStack = technologyStack.map((tech, index) => <li key={index}>{tech}</li>);
+    const techStack = technologyStack.map((tech, index) => <li className='techItem' key={index}>{tech}</li>);
   return (
     <>
         {/* <Modal /> */}
@@ -41,6 +41,38 @@ const Div = styled.div`
     align-items: center;
 
     .innerContent {
+        display: block;
+        width: 100%;
+        max-width:400px;
+        max-height: 500px;
+        padding: 30px 20px;
+        background: #fff;
+        color: #000;
+        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
 
+        h4 {
+            text-align: center;
+            font-size: 1.1em;
+            width: 100%;
+            padding-bottom: 20px;
+            font-weight: 600;
+        }
+
+        h5 {
+            text-align: left;
+            font-weight: 600;
+            font-size: 1em;
+        }
+
+        ul {
+            padding: 0;
+            margin: 0;
+            text-align: left;
+            font-weight: 300;
+        }
+        
+        li.techItem {
+            font-weight: 300;
+        }
     }
 `;
