1cfbed75de3489748bae86082244db4fcca730c4
standen1 <standen.sean@gmail.com>
2022-11-02 22:41:33 +0000

    Finished portfolioPopover element.

--- components/ui/PortfolioPopover/PortfolioPopover.tsx
+++ components/ui/PortfolioPopover/PortfolioPopover.tsx
@@ -3,29 +3,35 @@ import styled from 'styled-components';
 
 import Modal from "../Modal/Modal";
 import Button from '../Button/Button';
+import CloseIcon from '../CloseIcon/CloseIcon';
 
 export interface PortfolioPopoverProps {
     title: string;
     technologyStack: Array<string>;
     siteUrl: string;
     codeUrl?: string;
+    close: any;
 }
 
-export default function PortfolioPopover({title, technologyStack, siteUrl, codeUrl}: PortfolioPopoverProps) {
+export default function PortfolioPopover({title, technologyStack, siteUrl, codeUrl, close}: PortfolioPopoverProps) {
     const techStack = technologyStack.map((tech, index) => <li className='techItem' key={index}>{tech}</li>);
   return (
     <>
-        {/* <Modal /> */}
+        <Modal />
         <Div>
-            <div className='innerContent'>
-                <h4>{title}</h4>
-                <h5>Technology Stack</h5>
-                <ul>
-                    { techStack }
-                </ul>
-
-                <Button background='primary' onClick={() => console.log('Youclicked')} linkUrl="https://stackoverflow.com/questions/47616355/foreach-in-react-jsx-does-not-output-any-html" >Visit</Button>
-                <Button background='secondary' onClick={() => 'clicked.'} linkUrl="https://stackoverflow.com/questions/47616355/foreach-in-react-jsx-does-not-output-any-html">Source Code</Button>
+            <div className="popover">
+                <CloseIcon onClick={close} color="#000" />
+                <div className='innerContent'>
+                    <h4>{title}</h4>
+                    <h5>Technology Stack</h5>
+                    <ul>
+                        { techStack }
+                    </ul>
+                    <div className='buttonWrapper'>
+                        <Button background='primary' onClick={() => console.log('Youclicked')} linkUrl="https://stackoverflow.com/questions/47616355/foreach-in-react-jsx-does-not-output-any-html" >Visit</Button>
+                        <Button background='secondary' onClick={() => 'clicked.'} linkUrl="https://stackoverflow.com/questions/47616355/foreach-in-react-jsx-does-not-output-any-html">Source Code</Button>
+                    </div>
+                </div>
             </div>
         </Div>
     </>
@@ -40,7 +46,8 @@ const Div = styled.div`
     justify-content: center;
     align-items: center;
 
-    .innerContent {
+    .popover {
+        position: relative;
         display: block;
         width: 100%;
         max-width:400px;
@@ -50,9 +57,15 @@ const Div = styled.div`
         color: #000;
         box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
 
+        .innerContent {
+            display: block;
+            max-width: 250px;
+            margin: auto;
+        }
+
         h4 {
             text-align: center;
-            font-size: 1.1em;
+            font-size: 1.2em;
             width: 100%;
             padding-bottom: 20px;
             font-weight: 600;
@@ -61,7 +74,17 @@ const Div = styled.div`
         h5 {
             text-align: left;
             font-weight: 600;
-            font-size: 1em;
+            font-size: 1.1em;
+        }
+
+        .buttonWrapper {
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            gap: 15px;
+            margin-top: 25px
         }
 
         ul {
@@ -73,6 +96,7 @@ const Div = styled.div`
         
         li.techItem {
             font-weight: 300;
+            font-size: 1.1em;
         }
     }
 `;
