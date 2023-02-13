b3eaadfde22c1243bc707126ee27e303013cabf5
standen1 <standen.sean@gmail.com>
2023-02-13 23:18:13 +0000

    Attempted fix for bug.  Turns out it is an issue with NextJS, Styled Components, and Motion.

--- components/ui/Navigation/NavigationWrapper.tsx
+++ components/ui/Navigation/NavigationWrapper.tsx
@@ -7,29 +7,42 @@ import NavMenuMobile from './NavMenuMobile/NavMenuMobile';
 import MenuToggle from './MenuToggle/MenuToggle';
 import Modal from '../Modal/Modal';
 
+function getWindowWidth () {
+    let innerWidth;
+    if (typeof window !== "undefined") {
+        innerWidth = window.innerWidth;
+    } else {
+        innerWidth = 900;
+    }
+    return innerWidth;
+}
+
 export default function NavigationWrapper() {
     const [ navBarOpen, setNavBarOpen ] = useState(false);
+    const [ innerWidth, setInnerWidth ] = useState(getWindowWidth);
     const [ isMobile, setIsMobile ] = useState(false);
 
     const closeNavBar = () => setNavBarOpen(false);
 
-    const handleResize = () => {
-        if (window.innerWidth < 900) {
-            setIsMobile(true);
-        } else {
-            setIsMobile(false);
-        }
-    }
-
     useEffect(() => {
+        const handleResize = () => {
+            if (innerWidth < 900) {
+                setIsMobile(true);
+            } else {
+                setIsMobile(false);
+            }
+        }
         handleResize();
-        window.addEventListener('resize', handleResize);
-    }, [])
+        window.addEventListener('resize', () => {
+            const newWindowWidth = getWindowWidth();
+            setInnerWidth(newWindowWidth);
+            handleResize();
+        }) ;
+    }, [innerWidth])
 
     const mobileMenu = (
         <motion.div variants={navWrapperLoadAnimation}>
             <MenuToggle toggle={() => setNavBarOpen(!navBarOpen)} isOpen={navBarOpen} />
-            {/* <NavMenuDesktop /> */}
             <NavMenuMobile isOpen={navBarOpen} closeNavBar={closeNavBar} />
             {navBarOpen && <Modal close={closeNavBar} />}
         </motion.div>
